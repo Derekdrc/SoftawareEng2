f175706e97546eabd770bd238f46880cfc197c30
Derekdrc <33431199+Derekdrc@users.noreply.github.com>
2023-01-26 15:21:23 -0500

    Added comments

--- HW2SciCalculator/ScientificCalculator.py
+++ HW2SciCalculator/ScientificCalculator.py
@@ -28,26 +28,32 @@ def sin(num):
 
 
 def cos(num):
+    """Naturally uses degrees, have to convert if want radians"""
     return math.cos(num)
 
 
 def tan(num):
+    """Naturally uses degrees, have to convert if want radians"""
     return math.tan(num)
 
 
 def log(num, base):
+    """Uses math.py log function"""
     return math.log(num, base)
 
 
 def power(num, exponent):
+    """Uses math.py power function"""
     return math.pow(num, exponent)
 
 
 def square_root(num):
+    """Uses math.py square root function"""
     return math.sqrt(num)
 
 
 def factorial(num):
+    """Uses math.py factorial function"""
     return math.factorial(num)
 
 
@@ -70,27 +76,33 @@ def main():
                 Enter '11' to find the factorial of a number 
                 Enter '12' to end program: '''))
         except ValueError:
+            """Catches menu choice error that was not an integer character"""
             print("Not valid input. Please try again.")
             continue
         if user_choice == 1:
+            """Calls addition with given numbers"""
             num1 = float(input("Please enter the first number you wish to add: "))
             num2 = float(input("Please enter the second number you wish to add: "))
             print(f" {num1} + {num2} is {addition(num1, num2)}")
         elif user_choice == 2:
+            """Calls Subtraction with given numbers"""
             num1 = float(input("Please enter the first number you wish to subtract: "))
             num2 = float(input("Please enter the second number you wish to subtract: "))
             print(f" {num1} - {num2} is {subtraction(num1, num2)}")
         elif user_choice == 3:
+            """Calls multiplication with given numbers"""
             num1 = float(input("Please enter the first number you wish to multiply: "))
             num2 = float(input("Please enter the second number you wish to multiply: "))
             print(f" {num1} * {num2} is {multiplication(num1, num2)}")
         elif user_choice == 4:
+            """Calls division with given numbers, checks 0 us not denominator"""
             num1 = float(input("Please enter the first number you wish to divide: "))
             num2 = float(input("Please enter the second number you wish to divide: "))
             while num2 == 0:
                 num2 = float(input("The denominator can not be 0. Please enter the second number you wish to divide: "))
             print(f" {num1} / {num2} is {division(num1, num2)}")
         elif user_choice == 5:
+            """Calls sin with given numbers, checks for degrees or radians and converts"""
             num = float(input("Please enter the number you wish to find the sin of: "))
             degrees = int(input("If this number is in degrees press '1', if it is in radians press '2': "))
             if (degrees == 1):
@@ -100,6 +112,7 @@ def main():
             else:
                 print("Not a valid input. Please try again")
         elif user_choice == 6:
+            """Calls cos with given numbers, checks for degrees or radians and converts"""
             num = float(input("Please enter the number you wish to find the cos of: "))
             degrees = int(input("If this number is in degrees press '1', if it is in radians press '2': "))
             if (degrees == 1):
@@ -109,6 +122,7 @@ def main():
             else:
                 print("Not a valid input. Please try again")
         elif user_choice == 7:
+            """Calls tan with given numbers, checks for degrees or radians and converts"""
             num = float(input("Please enter the number you wish to find the tan of: "))
             degrees = int(input("If this number is in degrees press '1', if it is in radians press '2': "))
             if (degrees == 1):
@@ -118,23 +132,29 @@ def main():
             else:
                 print("Not a valid input. Please try again")
         elif user_choice == 8:
+            """Calls log function with given number and base"""
             num = float(input("Please enter the number you wish to calculate the log of: "))
             base = float(input("Please enter the log base you wish to use: "))
             print(f"The log of {num} with base {base} is {log(num,base)}")
         elif user_choice == 9:
+            """Calls exponent function with given base and power"""
             num = float(input("Please enter the number you wish to raise to a power: "))
             exponent = float(input("Please enter the exponent you wish to use: "))
             print(f"{num} to the power of {exponent} is {power(num,exponent)}")
         elif user_choice == 10:
+            """Calls square root function with given number"""
             num = float(input("Please enter the number you wish to take the square root of: "))
             print(f"The square root of {num} is {square_root(num)}")
         elif user_choice == 11:
+            """Calls factorial function with given integer"""
             num = int(input("Please enter the number you wish to find the factorial of: "))
             print(f"{num} factorial is {factorial(num)}")
         elif user_choice == 12:
+            """Exits"""
             print("Goodbye.")
             break
         else:
+            """Catches menu choice of an integer that was not an option"""
             print("Not valid input. Please try again.")
             continue
 
