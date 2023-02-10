476dccbea2e66c62c314e0a33b3913b10fb038b6
Derekdrc <33431199+Derekdrc@users.noreply.github.com>
2023-02-09 22:04:59 -0500

    Fleshed out other pages, worked on divide by 0

--- HW3CalculatorGUI/CalculatorGUI.py
+++ HW3CalculatorGUI/CalculatorGUI.py
@@ -58,6 +58,10 @@ class main_page(tk.Frame):
 
         def button_clear():
             e.delete(0, END)
+            button_0.configure(bg='cyan', command=lambda: button_click(0))
+
+        def do_nothing():
+            return
 
         def button_add():
             first_number = e.get()
@@ -90,6 +94,7 @@ class main_page(tk.Frame):
             math = "divide"
             f_num = int(first_number)
             e.delete(0, END)
+            button_0.configure(bg='red', command=lambda: do_nothing)
 
         def button_equal():
             second_number = e.get()
@@ -105,7 +110,7 @@ class main_page(tk.Frame):
                 e.insert(0, f_num * int(second_number))
 
             elif math == "divide":
-                e.insert(0, f_num / int(second_number))
+                e.insert(0, str(round(f_num / int(second_number), 6)))
 
         def button_click(num):
             box = e.get()
@@ -118,16 +123,16 @@ class main_page(tk.Frame):
 
         # define buttons
 
-        button_1 = Button(self, text="1", padx=40, pady=20, command=lambda: button_click(1))
-        button_2 = Button(self, text="2", padx=40, pady=20, command=lambda: button_click(2))
-        button_3 = Button(self, text="3", padx=40, pady=20, command=lambda: button_click(3))
-        button_4 = Button(self, text="4", padx=40, pady=20, command=lambda: button_click(4))
-        button_5 = Button(self, text="5", padx=40, pady=20, command=lambda: button_click(5))
-        button_6 = Button(self, text="6", padx=40, pady=20, command=lambda: button_click(6))
-        button_7 = Button(self, text="7", padx=40, pady=20, command=lambda: button_click(7))
-        button_8 = Button(self, text="8", padx=40, pady=20, command=lambda: button_click(8))
-        button_9 = Button(self, text="9", padx=40, pady=20, command=lambda: button_click(9))
-        button_0 = Button(self, text="0", padx=40, pady=20, command=lambda: button_click(0))
+        button_1 = Button(self, text="1", bg='cyan', padx=40, pady=20, command=lambda: button_click(1))
+        button_2 = Button(self, text="2", bg='cyan', padx=40, pady=20, command=lambda: button_click(2))
+        button_3 = Button(self, text="3", bg='cyan', padx=40, pady=20, command=lambda: button_click(3))
+        button_4 = Button(self, text="4", bg='cyan', padx=40, pady=20, command=lambda: button_click(4))
+        button_5 = Button(self, text="5", bg='cyan', padx=40, pady=20, command=lambda: button_click(5))
+        button_6 = Button(self, text="6", bg='cyan', padx=40, pady=20, command=lambda: button_click(6))
+        button_7 = Button(self, text="7", bg='cyan', padx=40, pady=20, command=lambda: button_click(7))
+        button_8 = Button(self, text="8", bg='cyan', padx=40, pady=20, command=lambda: button_click(8))
+        button_9 = Button(self, text="9", bg='cyan', padx=40, pady=20, command=lambda: button_click(9))
+        button_0 = Button(self, text="0", bg='cyan', padx=40, pady=20, command=lambda: button_click(0))
 
         button_clear = Button(self, text="Clear", padx=40, pady=20, command=button_clear)
         button_equal = Button(self, text="=", padx=40, pady=20, command=button_equal)
@@ -279,6 +284,12 @@ class settings_page(tk.Frame):
 
         main_page_button.grid(row=1, column=3, padx=10, pady=10)
 
+        degrees_button = ttk.Button(self, text="Degrees")
+        radians_button = ttk.Button(self, text="Radians")
+
+        degrees_button.grid(row=2, column=0, columnspan=2)
+        radians_button.grid(row=2, column=2, columnspan=2)
+
 
 class temperature_page(tk.Frame):
     # page that handles temperature conversions
@@ -296,7 +307,7 @@ class temperature_page(tk.Frame):
 
         # putting the button in its place by
         # using grid
-        settings_button.grid(row=1, column=1, padx=10, pady=10)
+        settings_button.grid(row=1, column=0, padx=10, pady=10)
 
         # button to show frame 2 with text layout2
         currency_button = ttk.Button(self, text="Currency Exchange",
@@ -304,13 +315,39 @@ class temperature_page(tk.Frame):
 
         # putting the button in its place by
         # using grid
-        currency_button.grid(row=1, column=2, padx=10, pady=10)
+        currency_button.grid(row=1, column=1, padx=10, pady=10)
 
         # main page button and add to grid
         main_page_button = ttk.Button(self, text="Main Calculator",
                                       command=lambda: controller.show_frame(main_page))
 
-        main_page_button.grid(row=1, column=3, padx=10, pady=10)
+        main_page_button.grid(row=1, column=2, padx=10, pady=10)
+
+        # create dropdowns for choosing temp conversions
+        options = [
+            "Farenheit",
+            "Celsius",
+            "Kelvin",
+        ]
+
+        user_input_box = Entry(self, width=15, borderwidth=5, font=('Arial', 10))
+        user_input_box.grid(row=2, column=0)
+
+        first_clicked = StringVar()
+        first_clicked.set("Temp")
+        first_drop_menu = OptionMenu(self, first_clicked, *options)
+        first_drop_menu.grid(row=2, column=1)
+
+        display_box = Entry(self, width=15, borderwidth=5, font=('Arial', 10), state=tk.DISABLED)
+        display_box.grid(row=2, column=2)
+
+        second_clicked = StringVar()
+        second_clicked.set("Temp")
+        displayed_drop_menu = OptionMenu(self, second_clicked, *options)
+        displayed_drop_menu.grid(row=2, column=3)
+
+        calculate_button = ttk.Button(self, text="Calculate")
+        calculate_button.grid(row=3, column=0, columnspan=4)
 
 
 class currency_page(tk.Frame):
@@ -329,7 +366,7 @@ class currency_page(tk.Frame):
 
         # putting the button in its place by
         # using grid
-        settings_button.grid(row=1, column=1, padx=10, pady=10)
+        settings_button.grid(row=1, column=0, padx=10, pady=10)
 
         # button to show frame 2 with text layout2
         temperature_button = ttk.Button(self, text="Temperature Conversion",
@@ -337,16 +374,55 @@ class currency_page(tk.Frame):
 
         # putting the button in its place by
         # using grid
-        temperature_button.grid(row=1, column=2, padx=10, pady=10)
+        temperature_button.grid(row=1, column=1, padx=10, pady=10)
 
         # main page button and add to grid
         main_page_button = ttk.Button(self, text="Main Calculator",
                                       command=lambda: controller.show_frame(main_page))
 
-        main_page_button.grid(row=1, column=3, padx=10, pady=10)
+        main_page_button.grid(row=1, column=2, padx=10, pady=10)
+
+        # create dropdowns for choosing currency conversions
+        options = [
+            "US Dollar",
+            "Pound",
+            "Yen",
+            "Cad",
+            "BTC"
+        ]
+
+        user_input_box = Entry(self, width=15, borderwidth=5, font=('Arial', 10))
+        user_input_box.grid(row=2, column=0)
+
+        first_clicked = StringVar()
+        first_clicked.set("Currency")
+        first_drop_menu = OptionMenu(self, first_clicked, *options)
+        first_drop_menu.grid(row=2, column=1)
+
+        display_box = Entry(self, width=15, borderwidth=5, font=('Arial', 10), state=tk.DISABLED)
+        display_box.grid(row=2, column=2, padx=0)
+
+        second_clicked = StringVar()
+        second_clicked.set("Currency")
+        displayed_drop_menu = OptionMenu(self, second_clicked, *options)
+        displayed_drop_menu.grid(row=2, column=3, padx=0)
+
+        calculate_button = ttk.Button(self, text="Calculate")
+        calculate_button.grid(row=3, column=0, columnspan=4)
 
 
 # driver code
 root = page_container()
 root.geometry("440x460")
 root.mainloop()
+
+
+# notes:
+#
+#
+#
+#
+#
+#
+#
+#
