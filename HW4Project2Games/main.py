e6c762d7509694a5bc04af591203b731e5cf2a58
Derekdrc <33431199+Derekdrc@users.noreply.github.com>
2023-02-28 22:07:01 -0500

    Initial commit

---
+++ HW4Project2Games/main.py
@@ -0,0 +1,9 @@
+"""
+name: Derek D'Arcy
+Description: This program is the gui for project 2 which will have RPS and TICTACTOE
+"""
+
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
